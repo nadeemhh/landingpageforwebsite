ebd49d2f7adb9e65ec45a42a6c461a53ba28eeb3
nadeemhh <flow669900@gmail.com>
2023-06-27 17:09:03 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const  fs = require('fs');
+const mongoose = require('mongoose')
 const app = express()
 app.use(express.json())
 let cors = require('cors')
@@ -10,163 +11,75 @@ app.use(express.static(publicDirectoryPath))
 const port = process.env.PORT || 3600
 
 
+mongoose.connect('mongodb+srv://virtual-trading:hkiyygh68tfgcfhs586@cluster0.ohx5a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+
+const CountSchema = new mongoose.Schema({
+  language_conversion_count: {
+    type: Number
+  },
+  visiter_count: {
+    type: Number
+  }
+});
+
+const Count = mongoose.model('Count', CountSchema);
+
+
 
  app.post('/visticount', (req, res) => {
-    console.log(req.body)
+  
+async function update() {
+  const count = await Count.findOne();
 
-  fs.readFile('user-footprint.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  
-    // Parse the JSON data into an object
-    const jsonData = JSON.parse(data);
-  
-    // Update the visiter_count
-    jsonData.visiter_count += req.body.visti_count;
-  
-    // Convert the object back to JSON string
-    const updatedData = JSON.stringify(jsonData, null, 2);
-  
-    // Write the updated JSON string back to the file
-    fs.writeFile('user-footprint.json', updatedData, 'utf8', (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-  
-      console.log('visiter_count updated successfully');
-      res.send('done')
-    });
-  });
+count.visiter_count += req.body.visti_count;
+await count.save();
+
+res.send('done')
+}
+update()
+
    });
 
 
 
    app.post('/languageConversionCount', (req, res) => {
-    console.log(req.body)
 
-  fs.readFile('language-conversion-count.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  
-    // Parse the JSON data into an object
-    const jsonData = JSON.parse(data);
-  
-    // Update the visiter_count
-    jsonData.language_conversion_count += req.body.languageConversionCount;
-  
-    // Convert the object back to JSON string
-    const updatedData = JSON.stringify(jsonData, null, 2);
-  
-    // Write the updated JSON string back to the file
-    fs.writeFile('language-conversion-count.json', updatedData, 'utf8', (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-  
-      console.log('language-Conversion-Count updated successfully');
-      res.send('done')
-    });
-  });
+async function update() {
+  const count = await Count.findOne();
+count.language_conversion_count += req.body.languageConversionCount;
+await count.save();
+
+res.send('done')
+}
+update()
+
    });
 
 
 
    app.get('/getcount', (req, res) => {
 
-    let obj={language_conversion_count: 0,visiter_count: 0}
-
- fs.readFile('user-footprint.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  
-    // Parse the JSON data into an object
-    const jsonData = JSON.parse(data);
-  obj.visiter_count=jsonData.visiter_count
-
-  fs.readFile('language-conversion-count.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  
-    // Parse the JSON data into an object
-    const jsonData = JSON.parse(data);
-    obj.language_conversion_count=jsonData.language_conversion_count
-    res.send(JSON.stringify(obj))
-  });
-
-
-  });
-
-  
+ 
+async function getc() {
+  const count = await Count.findOne();
+res.send(JSON.stringify(count))
+}
+getc()
+ 
    })
 
 
    app.get('/zero', (req, res) => {
 
-  
-
- fs.readFile('user-footprint.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  
-    // Parse the JSON data into an object
-    let jsonData = JSON.parse(data);
-jsonData.visiter_count=0;
-
-  // Convert the object back to JSON string
-  let updatedData = JSON.stringify(jsonData, null, 2);
-  
-  // Write the updated JSON string back to the file
-  fs.writeFile('user-footprint.json', updatedData, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    console.log('visiter_count updated successfully');
-   
-  });
-
-
-  fs.readFile('language-conversion-count.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  
-    // Parse the JSON data into an object
-    let jsonData = JSON.parse(data);
-jsonData.language_conversion_count=0;
-
-// Convert the object back to JSON string
-let updatedData = JSON.stringify(jsonData, null, 2);
-  
-// Write the updated JSON string back to the file
-fs.writeFile('language-conversion-count.json', updatedData, 'utf8', (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  console.log('language-Conversion-Count updated successfully');
+async function zero() {
+const count = await Count.findOne();
+count.visiter_count = 0;
+count.language_conversion_count = 0;
+await count.save();
+}
+zero()
   res.send('done')
-});
-
-  });
-
-
-  });
 
   
    })
